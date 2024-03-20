16175b8d8da9d39f5eee992eca383505ba481cce
Muzzammil Rafiq <mzmlrafiqcse@gmail.com>
2024-03-20 23:00:09 +0530

    email verification

---
+++ src/lib/mail.ts
@@ -0,0 +1,173 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const html = (link:string)=> `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Email Confirmation</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style type="text/css">
+      div[style*="margin: 16px 0;"] {
+        margin: 0 !important;
+      }
+      body {
+        width: 100% !important;
+        height: 100% !important;
+        padding: 0 !important;
+        margin: 0 !important;
+      }
+      table {
+        border-collapse: collapse !important;
+      }
+      a {
+        color: #1a82e2;
+      }
+    </style>
+  </head>
+  <body style="background-color: #e9ecef">
+   
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+        <td align="center" bgcolor="#e9ecef">
+          <table
+            border="0"
+            cellpadding="0"
+            cellspacing="0"
+            width="100%"
+            style="max-width: 600px"
+          >
+            <tr>
+              <td
+                align="left"
+                bgcolor="#ffffff"
+                style="
+                  padding: 24px;
+                  font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+                  font-size: 16px;
+                  line-height: 24px;
+                "
+              >
+                <p style="margin: 0">
+                  Tap the button below to confirm your email address. If you
+                  didn't create an account with
+                  <a href="https://blogdesire.com">Paste</a>, you can safely
+                  delete this email.
+                </p>
+              </td>
+            </tr>
+            <tr>
+              <td align="left" bgcolor="#ffffff">
+                <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                  <tr>
+                    <td align="center" bgcolor="#ffffff" style="padding: 12px">
+                      <table border="0" cellpadding="0" cellspacing="0">
+                        <tr>
+                          <td
+                            align="center"
+                            bgcolor="#1a82e2"
+                            style="border-radius: 6px"
+                          >
+                            <a
+                              href=${link}
+                              target="_blank"
+                              style="
+                                display: inline-block;
+                                padding: 16px 36px;
+                                font-family: 'Source Sans Pro', Helvetica, Arial,
+                                  sans-serif;
+                                font-size: 16px;
+                                color: #ffffff;
+                                text-decoration: none;
+                                border-radius: 6px;
+                              "
+                              >Confirm Email</a
+                            >
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td align="center" bgcolor="#e9ecef" style="padding: 24px">
+          <table
+            border="0"
+            cellpadding="0"
+            cellspacing="0"
+            width="100%"
+            style="max-width: 600px"
+          >
+            <tr>
+              <td
+                align="center"
+                bgcolor="#e9ecef"
+                style="
+                  padding: 12px 24px;
+                  font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+                  font-size: 14px;
+                  line-height: 20px;
+                  color: #666;
+                "
+              >
+                <p style="margin: 0">
+                  You received this email because we received a request for
+                  [type_of_action] for your account. If you didn't request
+                  [type_of_action] you can safely delete this email.
+                </p>
+              </td>
+            </tr>
+            <tr>
+              <td
+                align="center"
+                bgcolor="#e9ecef"
+                style="
+                  padding: 12px 24px;
+                  font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
+                  font-size: 14px;
+                  line-height: 20px;
+                  color: #666;
+                "
+              >
+                <p style="margin: 0">
+                  To stop receiving these emails, you can
+                  <a href="https://www.blogdesire.com" target="_blank"
+                    >unsubscribe</a
+                  >
+                  at any time.
+                </p>
+                <p style="margin: 0">
+                  Paste 1234 S. Broadway St. City, State 12345
+                </p>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
+`
+
+
+export const sendVerificationEmail = async (
+  email: string, 
+  token: string
+) => {
+  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+
+    
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Confirm your email",
+    html: html(confirmLink)
+  });
+};
