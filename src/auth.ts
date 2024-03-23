9aff3469209bc8e9b36954de58f9218fc98ccf0f
Muzzammil Rafiq <mzmlrafiqcse@gmail.com>
2024-03-23 10:54:42 +0530

    two factor authentication

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,12 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
 import { UserRole } from "@prisma/client";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 import { getUserById } from "@/data/user";
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
+
 export const {
   handlers: { GET, POST },
   auth,
@@ -18,21 +21,32 @@ export const {
     async linkAccount({ user }) {
       await db.user.update({
         where: { id: user.id },
-        data: { emailVerified: new Date() }
-      })
-    }
+        data: { emailVerified: new Date() },
+      });
+    },
   },
   callbacks: {
     async signIn({ user, account }) {
       // Allow OAuth without email verification
       if (account?.provider !== "credentials") return true;
 
-      const existingUser = await getUserById(user.id!);
+      const existingUser = await getUserById(user.id);
 
       // Prevent sign in without email verification
       if (!existingUser?.emailVerified) return false;
 
-      // TODO: Add 2FA check
+      if (existingUser.isTwoFactorEnabled) {
+        const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+          existingUser.id
+        );
+
+        if (!twoFactorConfirmation) return false;
+
+        // Delete two factor confirmation for next sign in
+        await db.twoFactorConfirmation.delete({
+          where: { id: twoFactorConfirmation.id },
+        });
+      }
 
       return true;
     },
@@ -57,7 +71,7 @@ export const {
       token.role = existingUser.role;
 
       return token;
-    }
+    },
   },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -2,7 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const html = (link:string)=> `<!DOCTYPE html>
+const html = (link: string) => `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8" />
@@ -154,32 +154,35 @@ const html = (link:string)=> `<!DOCTYPE html>
     </table>
   </body>
 </html>
-`
-export const sendPasswordResetEmail = async (
-  email: string,
-  token: string,
-) => {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`
+`;
+
+export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "2FA Code",
+    html: `<p>Your 2FA code: ${token}</p>`,
+  });
+};
+
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
     to: email,
     subject: "Reset your password",
-    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
+    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
   });
 };
 
-export const sendVerificationEmail = async (
-  email: string, 
-  token: string
-) => {
+export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
-    
   await resend.emails.send({
     from: "onboarding@resend.dev",
     to: email,
     subject: "Confirm your email",
-    html: html(confirmLink)
+    html: html(confirmLink),
   });
 };
