e7c859674480f97ec167df7446b0bd16a22f94dc
Muzzammil Rafiq <mzmlrafiqcse@gmail.com>
2024-03-23 10:35:30 +0530

    reset password token

--- src/actions/new-verification.ts
+++ src/actions/new-verification.ts
@@ -8,19 +8,19 @@ export const newVerification = async (token: string) => {
   const existingToken = await getVerificationTokenByToken(token);
 
   if (!existingToken) {
-    return { error: "Token does not exist!" };
+    return { error: "Token does not exist!",status:405 };
   }
 
   const hasExpired = new Date(existingToken.expires) < new Date();
 
   if (hasExpired) {
-    return { error: "Token has expired!" };
+    return { error: "Token has expired!",status:405 };
   }
 
   const existingUser = await getUserByEmail(existingToken.email);
 
   if (!existingUser) {
-    return { error: "Email does not exist!" };
+    return { error: "Email does not exist!",status:404 };
   }
 
   await db.user.update({
@@ -31,9 +31,12 @@ export const newVerification = async (token: string) => {
     }
   });
 
-  await db.verificationToken.delete({
-    where: { id: existingToken.id }
-  });
+  //in react.strict mode verification-component runs twice so it gives error
 
-  return { success: "Email verified!" };
+
+  // await db.verificationToken.delete({
+  //   where: { id: existingToken.id }
+  // });
+
+  return { success: "Email verified!" ,status:200};
 };
